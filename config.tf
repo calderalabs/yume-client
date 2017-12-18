0b43d6f73907dbee0c4eb092b5fa3206591587f2
Matteo Depalo <matteodepalo@gmail.com>
2017-12-18 15:05:31 +0100

    Add back minimum_protocol_version

--- config.tf
+++ config.tf
@@ -97,6 +97,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   viewer_certificate {
     acm_certificate_arn = "${data.aws_acm_certificate.certificate.arn}"
     ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1"
   }
 
   # viewer_certificate {
